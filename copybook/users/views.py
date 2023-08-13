fb41ced3882542e46787a8044b9f97bae9df0f7d
HalilovVlad <Halilovvlad00@mail.ru>
2023-08-14 02:56:21 +0500

    Описание

--- copybook/users/views.py
+++ copybook/users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
 
 
 def register(request):
     """Регистрация нового пользователя."""
     if request.method != 'POST':
         # Выводит пустую форму регистрации
-        form = UserCreationForm()
+        form = SignUpForm()
     else:
         # Обработка заполненной формы
-        form = UserCreationForm(data=request.POST)
+        form = SignUpForm(data=request.POST)
 
         if form.is_valid():
             new_user = form.save()
